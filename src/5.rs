36cedce189ece46a266ee95f4afc631b0b09b3b0
solyanoitatyana671 <solyanoitatyana671@gmail.com>
2025-03-10 06:52:23 +0000

    Add 5.rs

---
+++ src/5.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x == 3 {
+        println!("x is equal to 3");
+    } else if x == 4 {
+        println!("x is equal to 4");
+    } else {
+        println!("x is neither 3 nor 4");
+    }
+}
