470bf2af20dac13726f39df13d13c945bf19f417
solyanoitatyana671 <solyanoitatyana671@gmail.com>
2025-05-17 05:56:17 +0000

    Add 33.rs

---
+++ src/33.rs
@@ -0,0 +1,10 @@
+// This is a very simple example of Rust's basic syntax and features.
+// For more complex examples, consider using libraries like std::io or futures.
+
+fn main() {
+    // Declare a variable to hold an integer.
+    let number = 42;
+
+    // Print the value of 'number' using println!
+    println!("The value of 'number' is {}", number);
+}
